ee89a30dad7289e4ca99d8448d1ce7bf1b88d25d
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2019-12-25 22:21:58 +0530

    Update addPolynomialInLL.c

--- college assignment/Assignment 2 - LinkedList/Programs/addPolynomialInLL.c
+++ college assignment/Assignment 2 - LinkedList/Programs/addPolynomialInLL.c
@@ -11,26 +11,28 @@ typedef struct node{
 
 void addPolynomialToLL(int coefficient, int power,Node **poly)
 {
-    Node *temp, *ptr; 
-    ptr = *poly; 
-    if(ptr == NULL) 
-    { 
-        temp =( Node*)malloc(sizeof( Node)); 
-        temp->coefficient = coefficient; 
-        temp->power = power; 
-        *poly = temp; 
-        temp->next = (Node*)malloc(sizeof(Node)); 
-        temp = temp->next; 
-        temp->next = NULL; 
-    } 
-    else
-    { 
-        temp->coefficient = coefficient; 
-        temp->power = power; 
-        temp->next = (Node*)malloc(sizeof( Node)); 
-        temp = temp->next; 
-        temp->next = NULL; 
-    } 
+    Node *temp,*ptr;
+        temp=(Node *)malloc(sizeof(Node));
+        
+        if(temp==NULL){
+                printf("\nOut of Memory Space:\n");
+                exit(0);
+        }
+
+        temp->coefficient=coefficient;
+        temp->power=power;
+        temp->next=NULL;
+        
+        if(*poly==NULL)
+                *poly=temp;
+        
+        else{
+                ptr=*poly;
+                while(ptr->next){
+                        ptr=ptr->next;
+                }
+                ptr->next=temp;
+        } 
 }
 
 void displayPolynomialInLL(Node *poly){
@@ -39,7 +41,7 @@ void displayPolynomialInLL(Node *poly){
 			printf("| %d | %d | -> ",ptr->coefficient, ptr->power);
 			ptr=ptr->next; 
 		}
-        printf("| %d | %d | -> NULL",ptr->coefficient, ptr->power);	
+        printf("| %d | %d | -> NULL\n",ptr->coefficient, ptr->power);	
 }
 
 void displayPolynomial(Node *poly){
@@ -48,54 +50,41 @@ void displayPolynomial(Node *poly){
 			printf("%d X %d + ",ptr->coefficient, ptr->power);
 			ptr=ptr->next; 
 		}
-        printf("%d X %d = 0",ptr->coefficient, ptr->power);	
+        printf("%d X %d = 0\n",ptr->coefficient, ptr->power);	
 }
 
 void addPolynomial(Node *poly1, Node *poly2 , Node *poly){
-    while(poly1->next && poly2->next) 
-    { 
-        // If power of 1st polynomial is greater then 2nd, then store 1st as it is 
-        // and move its pointer 
-        if(poly1->power > poly2->power) 
-        { 
+    while(poly1->next && poly2->next){ 
+        if(poly1->power > poly2->power){ 
             poly->power = poly1->power; 
             poly->coefficient = poly1->coefficient; 
             poly1 = poly1->next; 
         } 
           
-        // If power of 2nd polynomial is greater then 1st, then store 2nd as it is 
-        // and move its pointer 
-        else if(poly1->power < poly2->power) 
-        { 
+        else if(poly1->power < poly2->power){ 
             poly->power = poly2->power; 
             poly->coefficient = poly2->coefficient; 
             poly2 = poly2->next; 
         } 
           
-        // If power of both polynomial numbers is same then add their coefficients 
-        else
-        { 
+        else{ 
             poly->power = poly1->power; 
             poly->coefficient = poly1->coefficient+poly2->coefficient; 
             poly1 = poly1->next; 
             poly2 = poly2->next; 
         } 
           
-        // Dynamically create new node 
         poly->next = (Node *)malloc(sizeof(Node)); 
         poly = poly->next; 
         poly->next = NULL; 
     } 
-    while(poly1->next || poly2->next) 
-    { 
-        if(poly1->next) 
-        { 
+    while(poly1->next || poly2->next){ 
+        if(poly1->next){ 
             poly->power = poly1->power; 
             poly->coefficient = poly1->coefficient; 
             poly1 = poly1->next; 
         } 
-        if(poly2->next) 
-        { 
+        if(poly2->next){ 
             poly->power = poly2->power; 
             poly->coefficient = poly2->coefficient; 
             poly2 = poly2->next; 
@@ -113,7 +102,7 @@ int main()
     printf("Enter polynomials\n");
     f(i,0,2){
         int noOfX=0;
-        printf("Enter polynomial %d\n", (i+1));
+        printf("\nEnter polynomial %d\n", (i+1));
         printf("How many X will it have? ");
         scanf("%d",&noOfX);
         f(j,0,noOfX){
@@ -122,7 +111,7 @@ int main()
             scanf("%d",&coefficient);
             printf("\nPower \t");
             scanf("%d",&power);
-            addPolynomialToLL(coefficient,power,&Poly[i]);
+            addPolynomialToLL(coefficient,power,&(Poly[i]));
         }
         printf("Entered Polynomial is :-\n");
 	    displayPolynomialInLL(Poly[i]);
