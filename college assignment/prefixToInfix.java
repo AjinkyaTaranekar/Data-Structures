796813492aa25d602c3d93851b80720a80944900
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2020-01-13 11:56:42 +0530

    Create prefixToInfix.java

---
+++ college assignment/prefixToInfix.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+class StackUsingLinkedList
+{
+    private class Node{
+        String data;
+        Node next;
+    }
+    
+    Node top;
+    private int size=0;
+    
+    
+    public void push(String data){
+        Node temp = new Node();
+        if(temp == null){
+            System.out.println("Heap overflow\n");
+            return;
+        }
+        temp.data = data;
+        temp.next = top;
+        top = temp;
+        size++;
+    }
+    
+    public boolean isEmpty(){
+        if(top == null)
+            return true;
+        return false;
+    }
+    
+    public void pop(){
+        if(!isEmpty()){
+            top = top.next;
+            size--;
+        }
+        else{    
+            System.out.println("Stack underflow\n");
+            return;
+        }
+    }
+    
+    public String peek(){
+        if(isEmpty()){
+            System.out.println("Stack underflow\n");
+            System.exit(0);
+        }
+        return top.data;
+    }
+    
+    public String top(){
+        String topValue = peek();
+        pop();
+        return topValue;
+    }
+    
+    public int size(){
+        return size;
+    }
+    
+    public void display(){
+        Node temp = top;
+        while(temp != null){
+            System.out.print(temp.data + " " );
+            temp=temp.next;    
+        }
+    }
+}
+
+public class Main{
+    public static boolean isOperator(char operator){
+        switch(operator){
+            case '+' : return true;
+            case '-' : return true;
+            case '*' : return true;
+            case '/' : return true;
+        }
+        return false;
+    }
+    public static String prefixToInfix(String sentence) { 
+        StackUsingLinkedList stack = new StackUsingLinkedList();
+    
+        String temp = "";    
+        for(int i = sentence.length()-1; i >=0;  i--){
+            if(!isOperator(sentence.charAt(i)))
+                    stack.push(sentence.charAt(i)+"");
+            else
+                stack.push("(" + stack.top() + sentence.charAt(i) + stack.top() + ")");
+             
+        }
+        temp = stack.top();
+        return temp;
+    } 
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        String sentence = "/+2b-3a";
+        System.out.println(prefixToInfix(sentence));
+        
+    }
+}
